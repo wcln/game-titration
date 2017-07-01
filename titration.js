c85efd7107ac17532c2c2cb498065847b596c7f9
Colin Bernard <colinjbernard@hotmail.com>
2017-07-01 13:15:17 -0700

    indicator alert, droplet, text instructions, titrant limit, changing colours, gradient background

--- titration.js
+++ titration.js
@@ -16,6 +16,7 @@ var gameStarted = false;
 
 // liquid to be drawn
 var buretLiquid, flaskLiquid, indicatorLiquid;
+var buretLiquidMeniscusLeft, buretLiquidMeniscusRight;
 
 var buretDispensing = false;
 var buretLiquidY = 30;
@@ -27,6 +28,14 @@ var liquidFallingOriginalY = 455;
 var liquidFallingHeight = 1;
 
 var flaskLiquidOriginalY = 880;
+var indicatorDropOriginalX, indicatorDropOriginalY;
+
+var indicatorCounter = 0;
+
+var halfCircle;
+var halfCircle2;
+var halfCircleSize = 25;
+var halfCircleAlpha = 0;
 
 
 function init() {
@@ -48,6 +57,10 @@ function update(event) {
  	if (gameStarted) {
  		if (buretDispensing && buretLiquidY < 450) {
 
+ 			if (buretLiquidY > 150) {
+ 				endGame();
+ 			}
+
  			if (liquidFalling !== null) {
  				stage.removeChild(liquidFalling);
  			}
@@ -57,11 +70,13 @@ function update(event) {
  			var oldIndex = stage.getChildIndex(buretLiquid);
  			stage.removeChild(buretLiquid);
  			buretLiquid = new createjs.Shape();
- 			buretLiquid.graphics.beginFill("blue").drawRect(375, buretLiquidY, 100, 450 - buretLiquidY);
+ 			buretLiquid.graphics.beginFill("#97def4").drawRect(375, buretLiquidY, 100, 450 - buretLiquidY);
  			stage.addChildAt(buretLiquid, oldIndex);
 
  			// increase flask level
  			flaskLiquid.y -= 0.3 * flaskCounter;
+ 			halfCircle.y -= 0.3 * flaskCounter;
+ 			halfCircleSize += 0.1;
  			flaskCounter += 0.003;
 
  			// draw line of liquid falling
@@ -73,8 +88,24 @@ function update(event) {
  			}
 
 			liquidFalling = new createjs.Shape();
- 			liquidFalling.graphics.beginFill("blue").drawRect(liquidFallingOriginalX, liquidFallingOriginalY, 4, liquidFallingHeight);
+ 			liquidFalling.graphics.beginFill("#97def4").drawRect(liquidFallingOriginalX, liquidFallingOriginalY, 4, liquidFallingHeight);
  			stage.addChildAt(liquidFalling, stage.getChildIndex(flaskLiquid));	
+
+
+ 			// re draw half circle
+ 			if (halfCircleAlpha < 0.6) {
+ 				halfCircleAlpha += 0.01;
+ 			}
+ 			stage.removeChild(halfCircle);
+ 			halfCircle = new createjs.Shape();
+			halfCircle.graphics.beginFill("purple").arc(STAGE_WIDTH/2 - 10, flaskLiquid.y + flaskLiquidOriginalY, halfCircleSize, 2*Math.PI, Math.PI);
+			halfCircle.alpha = halfCircleAlpha;
+			stage.addChild(halfCircle);
+			stage.removeChild(halfCircle2);
+ 			halfCircle2 = new createjs.Shape();
+			halfCircle2.graphics.beginFill("purple").arc(STAGE_WIDTH/2 - 10, flaskLiquid.y + flaskLiquidOriginalY, halfCircleSize + 8, 2*Math.PI, Math.PI);
+			halfCircle2.alpha = halfCircleAlpha - 0.3;
+			stage.addChild(halfCircle2);
  		}
 	}
 
@@ -83,21 +114,35 @@ function update(event) {
 
 function endGame() {
  	gameStarted = false;
+
+ 	// complete titration
+ 	var flaskComplete = new createjs.Shape();
+	flaskComplete.graphics.beginFill("purple").drawRect(280, flaskLiquid.y + flaskLiquidOriginalY, 265, 500);
+	flaskComplete.alpha = 0;
+	stage.addChildAt(flaskComplete, stage.getChildIndex(flaskLiquid) + 1);
+	createjs.Tween.get(flaskComplete).to({alpha:1}, 800);
+	createjs.Tween.get(liquidFalling).to({y:(flaskLiquid.y + flaskLiquidOriginalY) - liquidFallingOriginalY}, 150)
+		.call(function() { stage.removeChild(liquidFalling); });
+	stage.addChild(buretStop);
+	stage.removeChild(buretGo);
 }
 
 function initGraphics() {
 
 
 	buretLiquid = new createjs.Shape();
-	buretLiquid.graphics.beginFill("blue").drawRect(380, 30, 90, 425);
+	buretLiquid.graphics.beginFill("#97def4").drawRect(380, 30, 90, 425);
 	stage.addChild(buretLiquid);
 
+	buretLiquidMeniscusLeft = new createjs.Shape();
+	buretLiquid
+
 	flaskLiquid = new createjs.Shape();
-	flaskLiquid.graphics.beginFill("red").drawRect(280, flaskLiquidOriginalY, 265, 500);
+	flaskLiquid.graphics.beginFill("#b5e5f4").drawRect(280, flaskLiquidOriginalY, 265, 500);
 	stage.addChild(flaskLiquid);
 
 	indicatorLiquid = new createjs.Shape();
-	indicatorLiquid.graphics.beginFill("green").drawRect(0, 0, 30, 150);
+	indicatorLiquid.graphics.beginFill("#8cfaff").drawRect(0, 0, 30, 150);
 	indicatorLiquid.regX = 15;
 	indicatorLiquid.regY = 75;
 	indicatorLiquid.x = 335;
@@ -105,10 +150,22 @@ function initGraphics() {
 	indicatorLiquid.rotation = -47;
 	stage.addChild(indicatorLiquid);
 
+
+
 	stage.addChild(background);
+
+	indicatorDropOriginalX = STAGE_WIDTH/2 - 40;
+	indicatorDropOriginalY = STAGE_HEIGHT/2 - 10;
+	indicatorDrop.x = indicatorDropOriginalX;
+	indicatorDrop.y = indicatorDropOriginalY;
+	indicatorDrop.alpha = 0;
+	stage.addChild(indicatorDrop);
+
 	stage.addChild(setup);
 	stage.addChild(buretStop);
 	stage.addChild(eyedrop);
+
+
 
 	initListeners();
 	gameStarted = true;	
@@ -117,34 +174,66 @@ function initGraphics() {
 
 function initListeners() {
 	buretStop.on("mousedown", function(event) {
-		stage.addChild(buretGo);
-		stage.removeChild(this);
-		buretDispensing = true;
+		if (indicatorCounter < 4) {
+			indicatorAlert.alpha = 0;
+			stage.addChild(indicatorAlert);
+			createjs.Tween.get(indicatorAlert).to({alpha:1}, 1000);
+		} else {
+			stage.addChild(buretGo);
+			stage.removeChild(this);
+			buretDispensing = true;
+
+			halfCircle = new createjs.Shape();
+			halfCircle.graphics.beginFill("purple").arc(STAGE_WIDTH/2 - 10, flaskLiquid.y + flaskLiquidOriginalY, halfCircleSize, 2*Math.PI, Math.PI);
+			halfCircle.alpha = 0;
+			stage.addChild(halfCircle);
+		}
 	});
 	buretGo.on("mousedown", function(event) {
 		stage.addChild(buretStop);
 		stage.removeChild(this);
 		buretDispensing = false;
-		stage.removeChild(liquidFalling);
+		halfCircleSize = 25;
+		createjs.Tween.get(liquidFalling).to({y:(flaskLiquid.y + flaskLiquidOriginalY) - liquidFallingOriginalY}, 150)
+			.call(function() { stage.removeChild(liquidFalling); });
+		createjs.Tween.get(halfCircle).to({alpha:0}, 2000).call(function(){stage.removeChild(halfCircle)});
+		createjs.Tween.get(halfCircle2).to({alpha:0}, 2000).call(function(){stage.removeChild(halfCircle2)});
+		halfCircleAlpha = 0;
 	});
 	eyedrop.on("click", function(event) {
-		stage.addChild(eyedropSqueezed);
-		stage.removeChild(this);
+		if (indicatorCounter < 5) {
 
-		dispenseIndicator();
+			indicatorCounter++;
 
-		setTimeout(function() {
-			stage.addChild(eyedrop);
-			stage.removeChild(eyedropSqueezed);
-		}, 500);
+			stage.addChild(eyedropSqueezed);
+			stage.removeChild(this);
+
+			dispenseIndicator();
+
+			setTimeout(function() {
+				stage.addChild(eyedrop);
+				stage.removeChild(eyedropSqueezed);
+			}, 500);
+		}
+
 	});
 }
 
 function dispenseIndicator() {
+
+	stage.removeChild(indicatorAlert);
+
+	indicatorDrop.x = indicatorDropOriginalX;
+	indicatorDrop.y = indicatorDropOriginalY;
+
+
+
 	createjs.Tween.get(indicatorLiquid).to({x: indicatorLiquid.x + 5, y: indicatorLiquid.y + 5, scaleY: indicatorLiquid.scaleY - 0.1}, 500)
 		.wait(400).call(function() {
 			flaskLiquid.y--;
 		});
+	createjs.Tween.get(indicatorDrop).to({alpha:1}, 400).to({y:flaskLiquid.y + flaskLiquidOriginalY}, 400).to({alpha:0}, 500);
+	
 }
 
 //////////////////////// PRELOADJS FUNCTIONS
@@ -152,6 +241,8 @@ function dispenseIndicator() {
 // bitmap variables
 var setup, buretGo, buretStop, eyedrop, eyedropSqueezed;
 var background;
+var indicatorDrop;
+var indicatorAlert;
 
 function setupManifest() {
  	manifest = [
@@ -182,6 +273,14 @@ function setupManifest() {
  		{
  			src: "images/background.png",
  			id: "background"
+ 		},
+ 		{
+ 			src: "images/water_drop.png",
+ 			id: "water_drop"
+ 		},
+ 		{
+ 			src: "images/indicator_alert.png",
+ 			id: "indicator_alert"
  		}
  	];
 }
@@ -212,6 +311,10 @@ function handleFileLoad(event) {
    		eyedropSqueezed = new createjs.Bitmap(event.result);
    	} else if (event.item.id == "background") {
    		background = new createjs.Bitmap(event.result);
+   	} else if (event.item.id == "water_drop") {
+   		indicatorDrop = new createjs.Bitmap(event.result);
+   	} else if (event.item.id == "indicator_alert") {
+   		indicatorAlert = new createjs.Bitmap(event.result);
    	}
 }
 
